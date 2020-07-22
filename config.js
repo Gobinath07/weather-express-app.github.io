c10f58283b96f17934d64dfe7cf52c5e484df5c8
gobinath <gobidyni07@gmail.com>
2020-07-22 13:18:27 +0100

    first

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "http://api.openweathermap.org/data/2.5/weather?zip=",
+        SECRET_KEY: "&appid=98721fd58c1442399314ce10c43bb3a7"
+    }
+}
+
+module.exports = constants;
